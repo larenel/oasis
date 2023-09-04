98e53c0e2eca0d98c90c6b76b5bed35e3063b962
Ilshat Sabirov <nenenel@yandex.ru>
2023-09-04 23:43:08 +0300

    pagination done

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,11 +1,13 @@
 import { getToday } from '../utils/helpers'
 import supabase from './supabase'
+import { PAGE_SIZE } from '../utils/constance'
 
-export const getBookings = async ({ filter, sortBy }) => {
+export const getBookings = async ({ filter, sortBy, page }) => {
   let query = supabase
     .from('bookings')
     .select(
-      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)'
+      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
+      { count: 'exact' }
     )
 
   if (filter) {
@@ -16,14 +18,20 @@ export const getBookings = async ({ filter, sortBy }) => {
     query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' })
   }
 
-  const { data, error } = await query
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE
+    const to = from + PAGE_SIZE - 1
+    query = query.range(from, to)
+  }
+
+  const { data, error, count } = await query
 
   if (error) {
     console.log(error)
     throw new Error('Bookings can not be loaded')
   }
 
-  return data
+  return { data, count }
 }
 
 export async function getBooking(id) {
