8ae1534b387132102c27fe72c25bf38fd828020e
Ilshat Sabirov <nenenel@yandex.ru>
2023-09-11 01:47:45 +0300

    netlify

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -77,7 +77,11 @@ const CabinRow = ({ cabin }) => {
           <Menus.Menu>
             <Menus.Toggle id={cabinId} />
             <Menus.List id={cabinId}>
-              <Menus.Button onClick={handleDuplicate} icon={<HiSquare2Stack />}>
+              <Menus.Button
+                onClick={handleDuplicate}
+                disabled={isCreating}
+                icon={<HiSquare2Stack />}
+              >
                 Duplicate
               </Menus.Button>
 
