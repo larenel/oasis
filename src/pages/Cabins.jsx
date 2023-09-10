d96ddfc0f7cb79635421268f36626cf8a8ca8c9f
Ilshat Sabirov <nenenel@yandex.ru>
2023-09-11 02:40:38 +0300

    casing import fix

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,6 @@
 import AddCabin from '../features/cabins/AddCabin'
 import CabinTable from '../features/cabins/CabinTable'
-import CabinTableOperations from '../features/cabins/cabinTableOperations'
+import CabinTableOperations from '../features/cabins/CabinTableOperations'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 
